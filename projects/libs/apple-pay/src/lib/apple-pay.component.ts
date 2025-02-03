a7365da3b77c35b43c692933ad2b5028ad2b7e64
Senad Osmic <senad.osmic@monri.com>
2025-02-03 15:17:47 +0100

    store update

--- projects/libs/apple-pay/src/lib/apple-pay.component.ts
+++ projects/libs/apple-pay/src/lib/apple-pay.component.ts
@@ -1,5 +1,4 @@
-import {Component, effect, ElementRef, inject, Input, OnInit, Renderer2} from '@angular/core';
-import {ApplePayButtonConfig} from "./models/apple-pay.models";
+import {Component, inject, Input} from '@angular/core';
 import {patchState} from "@ngrx/signals";
 import {ApplePayStore} from "./store/apple-pay.store";
 import {StartPaymentRequest} from "./interfaces/alternative-payment-method.interface";
@@ -12,105 +11,11 @@ import {ApplePayService} from "./services/apple-pay.service";
   styleUrls: ['./apple-pay.component.scss'],
   providers: [ApplePayStore, ApplePayService],
 })
-export class ApplePayComponent implements OnInit {
-
-  appleInputParams: ApplePayButtonConfig | undefined = undefined;
-
+export class ApplePayComponent {
   protected readonly applePayStore = inject(ApplePayStore)
 
   @Input() set inputParams(value: StartPaymentRequest) {
     patchState(this.applePayStore, {inputParams: value});
     console.log("Proba", this.applePayStore.inputParams());
-  }
-
-  constructor(private renderer: Renderer2, private el: ElementRef) {
-    effect(() => {
-      console.log("Proba", this.applePayStore.inputParams());
-    });
-  }
-
-  ngOnInit(): void {
-    this.loadApplePayScript().then(() => this.createApplePayButton());
-  }
-
-  private loadApplePayScript(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      console.log("Script is loaded.")
-      const script = this.renderer.createElement('script');
-      script.src =
-        'https://applepay.cdn-apple.com/jsapi/1.latest/apple-pay-sdk.js';
-      script.crossOrigin = 'anonymous';
-      script.onload = resolve;
-      script.onerror = reject;
-      this.renderer.appendChild(document.body, script);
-    });
-  }
-
-  private createApplePayButton(): void {
-    const applePayButton = this.renderer.createElement('apple-pay-button');
-    this.renderer.setAttribute(applePayButton, 'id', 'apple-pay-button');
-    this.renderer.setAttribute(applePayButton, 'buttonstyle', this.applePayStore.appleButtonStyle().buttonStyle || 'black');
-    this.renderer.setAttribute(applePayButton, 'type', this.applePayStore.appleButtonStyle().buttonType || 'buy');
-    this.renderer.setAttribute(applePayButton, 'locale', this.applePayStore.appleButtonStyle().locale || 'en-US');
-
-    this.renderer.appendChild(
-      this.el.nativeElement.querySelector('#container'),
-      applePayButton
-    );
-    console.log("Button is created.")
-    this.renderer.listen(
-      applePayButton,
-      'click',
-      this.onApplePayButtonClick.bind(this)
-    );
-  }
-
-  private onApplePayButtonClick(): void {
-    console.log('Button clicked');
-    const request = {
-      countryCode: this.appleInputParams?.countryCode || 'HR',
-      currencyCode: this.appleInputParams?.currencyCode || 'EUR',
-      supportedNetworks: this.appleInputParams?.supportedNetworks || ['visa', 'masterCard', 'amex', 'discover'],
-      merchantCapabilities: this.appleInputParams?.merchantCapabilities || ['supports3DS'],
-      total: {
-        label: this.appleInputParams?.totalLabel || "Parkmatix",
-        amount: this.appleInputParams?.totalAmount || "2.00",
-      }
-    };
-    console.log("Request:", request)
-
-    const session = new (window as any).ApplePaySession(3, request);
-    session.onvalidatemerchant = (event: { validationURL: any }) => {
-      const applePayInstance = {
-        performValidation: (
-          options: any,
-          callback: (
-            arg0: null,
-            arg1: {
-              merchantSession: string;
-            }
-          ) => void
-        ) => {
-          // Mock implementation of performValidation
-          callback(null, {merchantSession: 'validSessionData'});
-        }
-      };
-
-      applePayInstance.performValidation(
-        {
-          validationURL: event.validationURL,
-          displayName: 'My Store'
-        },
-        (err, merchantSession) => {
-          if (err) {
-            console.error('Apple Pay failed to load.');
-            return;
-          }
-          session.completeMerchantValidation(merchantSession);
-        }
-      );
-    };
-
-    session.begin();
   }
 }
--- projects/libs/apple-pay/src/lib/store/apple-pay.store.ts
+++ projects/libs/apple-pay/src/lib/store/apple-pay.store.ts
@@ -1,73 +1,167 @@
 import {
-    patchState,
-    signalStore,
-    StateSignal,
-    withComputed,
-    withHooks,
-    withMethods,
-    withState
+  patchState,
+  signalStore,
+  StateSignal,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState
 } from '@ngrx/signals';
-import {computed, inject} from '@angular/core';
+import {computed, ElementRef, inject, Renderer2} from '@angular/core';
 import {setPending, withRequestStatus} from './request-status.feature';
 import {Prettify} from '@ngrx/signals/src/ts-helpers';
 import {
-    MethodsDictionary,
-    SignalsDictionary,
-    SignalStoreSlices
+  MethodsDictionary,
+  SignalsDictionary,
+  SignalStoreSlices
 } from '@ngrx/signals/src/signal-store-models';
 import {StartPaymentRequest} from '../interfaces/alternative-payment-method.interface';
 
 export const ApplePayStore = signalStore(
-    withState({
-        environment: '',
-        inputParams: {
-            payment_method: '',
-            data: {}
-        } as StartPaymentRequest,
-        resolution: window.innerWidth
+  withState({
+    environment: '',
+    inputParams: {
+      payment_method: '',
+      data: {}
+    } as StartPaymentRequest,
+    resolution: window.innerWidth
+  }),
+  withRequestStatus(),
+  withComputed(store => ({
+    appleButtonStyle: computed(() => {
+      return {
+        buttonStyle: store.inputParams().data['buttonStyle'] || 'black',
+        buttonType: store.inputParams().data['buttonType'] || 'buy',
+        locale: store.inputParams().data['locale'] || 'en-US',
+      }
     }),
-    withRequestStatus(),
-    withComputed(store => ({
-        appleButtonStyle: computed(() => {
-            return {
-                buttonStyle: store.inputParams().data['buttonStyle'] || 'black',
-                buttonType: store.inputParams().data['buttonType'] || 'buy',
-                locale: store.inputParams().data['locale'] || 'en-US',
-            }
-        }),
-        isMobileView: computed(() => store.resolution() <= 768),
-        isLoading: computed(() => store.isPending())
-    })),
-    withMethods(
-        (
-            store,
-        ) => ({
-            setWindowServices() {
-                window.applePayStore = store;
-                // window.applePayService = applePayService;
-                // window.translationService = translationService;
-            },
+    isMobileView: computed(() => store.resolution() <= 768),
+    isLoading: computed(() => store.isPending())
+  })),
+  withMethods(
+    (
+      store,
+      renderer = inject(Renderer2),
+      el = inject(ElementRef)
+    ) => {
+      const loadApplePayScript = () => {
+        return new Promise((resolve, reject) => {
+          console.log("Script is loaded.")
+          const script = renderer.createElement('script');
+          script.src =
+            'https://applepay.cdn-apple.com/jsapi/1.latest/apple-pay-sdk.js';
+          script.crossOrigin = 'anonymous';
+          script.onload = resolve;
+          script.onerror = reject;
+          renderer.appendChild(document.body, script);
         })
-    ),
-    withHooks({
-        onInit(store) {
-            patchState(store, setPending());
-            store.setWindowServices();
-        }
-    })
+      }
+
+      const onApplePayButtonClick = () => {
+        const request = {
+          //   countryCode: this.appleInputParams?.countryCode || 'HR',
+          //   currencyCode: this.appleInputParams?.currencyCode || 'EUR',
+          //   supportedNetworks: this.appleInputParams?.supportedNetworks || ['visa', 'masterCard', 'amex', 'discover'],
+          //   merchantCapabilities: this.appleInputParams?.merchantCapabilities || ['supports3DS'],
+          //   total: {
+          //     label: this.appleInputParams?.totalLabel || "Parkmatix",
+          //     amount: this.appleInputParams?.totalAmount || "2.00",
+          //   }
+        };
+        // console.log("Request:", request)
+
+        const session = new (window as any).ApplePaySession(3, request);
+        session.onvalidatemerchant = (event: { validationURL: any }) => {
+          const applePayInstance = {
+            performValidation: (
+              options: any,
+              callback: (
+                arg0: null,
+                arg1: {
+                  merchantSession: string;
+                }
+              ) => void
+            ) => {
+              // Mock implementation of performValidation
+              callback(null, {merchantSession: 'validSessionData'});
+            }
+          };
+
+          applePayInstance.performValidation(
+            {
+              validationURL: event.validationURL,
+              displayName: 'My Store'
+            },
+            (err, merchantSession) => {
+              if (err) {
+                console.error('Apple Pay failed to load.');
+                return;
+              }
+              session.completeMerchantValidation(merchantSession);
+            }
+          );
+        };
+
+        session.begin();
+      }
+
+      const createApplePayButton = () => {
+        const applePayButton = renderer.createElement('apple-pay-button');
+        renderer.setAttribute(applePayButton, 'id', 'apple-pay-button');
+        renderer.setAttribute(applePayButton, 'buttonstyle', store.appleButtonStyle().buttonStyle || 'black');
+        renderer.setAttribute(applePayButton, 'type', store.appleButtonStyle().buttonType || 'buy');
+        renderer.setAttribute(applePayButton, 'locale', store.appleButtonStyle().locale || 'en-US');
+
+        renderer.appendChild(
+          el.nativeElement.querySelector('#container'),
+          applePayButton
+        );
+        console.log("Button is created.")
+        renderer.listen(
+          applePayButton,
+          'click',
+          onApplePayButtonClick.bind(this)
+        );
+      }
+
+      const onLoad = () => {
+        loadApplePayScript().then(() => {
+            createApplePayButton()
+          }
+        )
+      }
+      const setWindowServices = () => {
+        window.applePayStore = store;
+        // window.applePayService = applePayService;
+        // window.translationService = translationService;
+      }
+
+      return {
+        onLoad,
+        setWindowServices
+      }
+    }
+  ),
+  withHooks({
+    onInit(store) {
+      patchState(store, setPending());
+      store.onLoad();
+      store.setWindowServices();
+    }
+  })
 );
 
 declare global {
-    interface Window {
-        // applePayService: KeksPayService;
-        // translationService: TranslationService;
-        applePayStore:
-            | Prettify<
-            SignalStoreSlices<object> &
-            SignalsDictionary &
-            MethodsDictionary &
-            StateSignal<object>
-        >
-            | unknown;
-    }
+  interface Window {
+    // applePayService: KeksPayService;
+    // translationService: TranslationService;
+    applePayStore:
+      | Prettify<
+      SignalStoreSlices<object> &
+      SignalsDictionary &
+      MethodsDictionary &
+      StateSignal<object>
+    >
+      | unknown;
+  }
 }
